2e6559be922a5054e4e112f2f7837bdd924f8fc6
ThierryBeaulieu <thierry.abeaulieu@gmail.com>
2022-10-18 19:56:34 -0400

    fix: adding a recipe page

--- client/src/app/modules/app-routing.module.ts
+++ client/src/app/modules/app-routing.module.ts
@@ -4,12 +4,14 @@ import { AboutPageComponent } from '@app/pages/about-page/about-page.component';
 import { GamePageComponent } from '@app/pages/game-page/game-page.component';
 import { MainPageComponent } from '@app/pages/main-page/main-page.component';
 import { MaterialPageComponent } from '@app/pages/material-page/material-page.component';
+import { RecipesPageComponent } from '@app/pages/recipes-page/recipes-page.component';
 
 const routes: Routes = [
     { path: '', redirectTo: '/home', pathMatch: 'full' },
     { path: 'home', component: MainPageComponent },
     { path: 'game', component: GamePageComponent },
     { path: 'about', component: AboutPageComponent },
+    { path: 'recipes', component: RecipesPageComponent },
     { path: 'material', component: MaterialPageComponent },
     { path: '**', redirectTo: '/home' },
 ];
