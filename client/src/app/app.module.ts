1980241828ab8b902f3f74349a82cafc06533218
ThierryBeaulieu <thierry.abeaulieu@gmail.com>
2022-10-18 19:31:00 -0400

    fix: adding an about page

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -12,6 +12,7 @@ import { AppComponent } from '@app/pages/app/app.component';
 import { GamePageComponent } from '@app/pages/game-page/game-page.component';
 import { MainPageComponent } from '@app/pages/main-page/main-page.component';
 import { MaterialPageComponent } from '@app/pages/material-page/material-page.component';
+import { AboutPageComponent } from './about-page/about-page.component';
 
 /**
  * Main module that is used in main.ts.
@@ -28,6 +29,7 @@ import { MaterialPageComponent } from '@app/pages/material-page/material-page.co
         PlayAreaComponent,
         SidebarComponent,
         NavigationBarComponent,
+        AboutPageComponent,
     ],
     imports: [AppMaterialModule, AppRoutingModule, BrowserAnimationsModule, BrowserModule, FormsModule, HttpClientModule],
     providers: [],
