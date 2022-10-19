037f1ff7e4ecf77f8c87a8d3deadd63b41ec7b87
ThierryBeaulieu <thierry.abeaulieu@gmail.com>
2022-10-18 22:13:21 -0400

    fix: iterating successfuly through elementg

--- client/src/app/components/recipe-card/recipe-card.component.ts
+++ client/src/app/components/recipe-card/recipe-card.component.ts
@@ -1,27 +1,10 @@
-import { Component, OnInit } from '@angular/core';
-import { DEFAULT_RECIPES, DEFAULT_NB_STARS } from '@app/contants/default-recipes';
+import { Component, Input } from '@angular/core';
+import { Recipe } from '@app/interfaces/recipe';
 @Component({
     selector: 'app-recipe-card',
     templateUrl: './recipe-card.component.html',
     styleUrls: ['./recipe-card.component.scss'],
 })
-export class RecipeCardComponent implements OnInit {
-    title: string;
-    img: string;
-    ingredients: string[];
-    steps: string[];
-    stars: number[];
-    defaultNbStars: number;
-
-    ngOnInit(): void {
-        this.title = DEFAULT_RECIPES[0].title;
-        this.img = DEFAULT_RECIPES[0].img;
-        this.ingredients = DEFAULT_RECIPES[0].ingredients;
-        this.steps = DEFAULT_RECIPES[0].steps;
-        this.stars = [];
-        for (let i = 0; i < DEFAULT_RECIPES[0].stars; i++) {
-            this.stars.push(i);
-        }
-        this.defaultNbStars = DEFAULT_NB_STARS;
-    }
+export class RecipeCardComponent {
+    @Input() recipe: Recipe;
 }
