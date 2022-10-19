fc5ba17f3cf1aef879b5804af2645c0bfe6e28b5
ThierryBeaulieu <thierry.abeaulieu@gmail.com>
2022-10-19 14:51:57 -0400

    fix: adding a class that can mock socket.io

--- client/src/app/services/socket-client/socket-client.service.spec.ts
+++ client/src/app/services/socket-client/socket-client.service.spec.ts
@@ -1,4 +1,6 @@
 import { TestBed } from '@angular/core/testing';
+import { SocketTestHelper } from '@app/classes/socket-client-helper/socket-client.helper';
+import { Socket } from 'socket.io-client';
 
 import { SocketClientService } from './socket-client.service';
 
@@ -8,6 +10,8 @@ describe('SocketClientService', () => {
     beforeEach(() => {
         TestBed.configureTestingModule({});
         service = TestBed.inject(SocketClientService);
+        // eslint-disable-next-line prettier/prettier
+        service.socket = (new (SocketTestHelper) as unknown) as Socket;
     });
 
     it('should be created', () => {
