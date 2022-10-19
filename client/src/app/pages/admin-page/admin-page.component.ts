500a38e9e2693289e0844000763bbaf5431f5956
ThierryBeaulieu <thierry.abeaulieu@gmail.com>
2022-10-18 23:38:33 -0400

    fix: adding a component that can add recipes and delete them

--- client/src/app/pages/admin-page/admin-page.component.ts
+++ client/src/app/pages/admin-page/admin-page.component.ts
@@ -1,8 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { DEFAULT_RECIPES } from '@app/contants/default-recipes';
+import { Recipe } from '@app/interfaces/recipe';
 
 @Component({
     selector: 'app-admin-page',
     templateUrl: './admin-page.component.html',
     styleUrls: ['./admin-page.component.scss'],
 })
-export class AdminPageComponent {}
+export class AdminPageComponent implements OnInit {
+    allRecipes: Recipe[];
+    shownRecipes: Recipe[];
+
+    ngOnInit(): void {
+        this.allRecipes = DEFAULT_RECIPES;
+        this.shownRecipes = [];
+    }
+}
