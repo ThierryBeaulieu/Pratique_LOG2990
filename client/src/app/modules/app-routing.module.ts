fe2aef7f90982d950f246a6652dadc783ff9dfa5
ThierryBeaulieu <thierry.abeaulieu@gmail.com>
2022-10-18 20:01:03 -0400

    fix: adding a navigation page

--- client/src/app/modules/app-routing.module.ts
+++ client/src/app/modules/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AboutPageComponent } from '@app/pages/about-page/about-page.component';
+import { ForumPageComponent } from '@app/pages/forum-page/forum-page.component';
 import { GamePageComponent } from '@app/pages/game-page/game-page.component';
 import { MainPageComponent } from '@app/pages/main-page/main-page.component';
 import { MaterialPageComponent } from '@app/pages/material-page/material-page.component';
@@ -13,6 +14,7 @@ const routes: Routes = [
     { path: 'about', component: AboutPageComponent },
     { path: 'recipes', component: RecipesPageComponent },
     { path: 'material', component: MaterialPageComponent },
+    { path: 'forum', component: ForumPageComponent },
     { path: '**', redirectTo: '/home' },
 ];
 
