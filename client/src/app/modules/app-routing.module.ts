6eccf99524c2cf2260f754029291688652c138d8
ThierryBeaulieu <thierry.abeaulieu@gmail.com>
2022-10-18 20:18:16 -0400

    fix: routing of the admin page

--- client/src/app/modules/app-routing.module.ts
+++ client/src/app/modules/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AboutPageComponent } from '@app/pages/about-page/about-page.component';
+import { AdminPageComponent } from '@app/pages/admin-page/admin-page.component';
 import { ForumPageComponent } from '@app/pages/forum-page/forum-page.component';
 import { GamePageComponent } from '@app/pages/game-page/game-page.component';
 import { MainPageComponent } from '@app/pages/main-page/main-page.component';
@@ -15,6 +16,7 @@ const routes: Routes = [
     { path: 'recipes', component: RecipesPageComponent },
     { path: 'material', component: MaterialPageComponent },
     { path: 'forum', component: ForumPageComponent },
+    { path: 'admin', component: AdminPageComponent },
     { path: '**', redirectTo: '/home' },
 ];
 
