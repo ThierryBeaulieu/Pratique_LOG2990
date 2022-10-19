dab420414f928c42d333acfa9db1da973b98cda1
ThierryBeaulieu <thierry.abeaulieu@gmail.com>
2022-10-18 20:35:14 -0400

    fix: adding a default recipe

--- client/src/app/contants/default-recipes.ts
+++ client/src/app/contants/default-recipes.ts
@@ -1,1 +1,18 @@
-export const DEFAULT_RECIPES = [{}];
+import { Recipe } from '@app/interfaces/recipe';
+
+export const DEFAULT_RECIPES: Recipe[] = [
+    {
+        title: 'Salade de légumes',
+        img: '',
+        ingredients: ['Feuilles vertes', 'concombre', 'amandes', 'tomates cerises'],
+        steps: [
+            'Placer les feuilles vertes dans un bol',
+            'Épelucher le concombres',
+            'Couper les tomates cerises',
+            'Placer les tomates cerises dans le bol',
+            'Griller les amandes',
+            'Broyer les amandes',
+            'Mélanger le tout',
+        ],
+    },
+];
