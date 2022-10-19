f50c48435698efb70e581ac552d8cb2d5108e953
ThierryBeaulieu <thierry.abeaulieu@gmail.com>
2022-10-18 20:29:24 -0400

    fix: using the attribute title

--- client/src/app/components/recipe-card/recipe-card.component.ts
+++ client/src/app/components/recipe-card/recipe-card.component.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
 
+const DEFAULT_TITLE = 'Salade de légumes';
 @Component({
     selector: 'app-recipe-card',
     templateUrl: './recipe-card.component.html',
     styleUrls: ['./recipe-card.component.scss'],
 })
-export class RecipeCardComponent {}
+export class RecipeCardComponent {
+    title: string = DEFAULT_TITLE;
+}
