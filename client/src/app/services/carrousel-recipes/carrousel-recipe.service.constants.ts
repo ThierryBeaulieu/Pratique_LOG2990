c4c98a1c50b6777fba1ec8969709ca363c30c79d
ThierryBeaulieu <thierry.abeaulieu@gmail.com>
2022-10-19 12:30:09 -0400

    feat: adding a spy object with jasmin

---
+++ client/src/app/services/carrousel-recipes/carrousel-recipe.service.constants.ts
@@ -0,0 +1,21 @@
+import { Recipe } from '@app/interfaces/recipe';
+
+const firstRecipe: Recipe = {
+    id: '0',
+    name: 'firstRecipe',
+    img: '',
+    ingredients: [],
+    steps: [],
+    stars: 2,
+};
+
+const secondRecipe: Recipe = {
+    id: '1',
+    name: 'secondRecipe',
+    img: '',
+    ingredients: [],
+    steps: [],
+    stars: 3,
+};
+
+export const BASIC_TESTING_RECIPES: Recipe[] = [firstRecipe, secondRecipe];
--- client/src/app/services/carrousel-recipes/carrousel-recipes.service.spec.ts
+++ client/src/app/services/carrousel-recipes/carrousel-recipes.service.spec.ts
@@ -1,16 +1,27 @@
 import { TestBed } from '@angular/core/testing';
-
-import { CarrouselRecipesService } from './carrousel-recipes.service';
+import { BASIC_TESTING_RECIPES } from '@app/services/carrousel-recipes/carrousel-recipe.service.constants';
+import { CarrouselRecipesService } from '@app/services/carrousel-recipes/carrousel-recipes.service';
+import { RecipeService } from '@app/services/recipe.service.ts/recipe.service';
 
 describe('CarrouselRecipesService', () => {
     let service: CarrouselRecipesService;
+    let spyRecipeService: jasmine.SpyObj<RecipeService>;
 
     beforeEach(() => {
+        spyRecipeService = jasmine.createSpyObj<RecipeService>('RecipeService', [
+            'addRecipe',
+            'getAllRecipes',
+            'getRecipeByIndex',
+            'isIndexOutOfRange',
+        ]);
+        spyRecipeService.getAllRecipes.and.callFake(() => {
+            return BASIC_TESTING_RECIPES;
+        });
         TestBed.configureTestingModule({});
         service = TestBed.inject(CarrouselRecipesService);
     });
 
-    it('should be created', () => {
+    it('CarrouselRecipesService should be created', () => {
         expect(service).toBeTruthy();
     });
 
