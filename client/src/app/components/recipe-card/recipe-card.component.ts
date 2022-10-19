3dbbe57457c24b8ea79c4c9be60164020c9b1262
ThierryBeaulieu <thierry.abeaulieu@gmail.com>
2022-10-18 20:41:28 -0400

    fix: adding the initialisation of the recipe card componenet

--- client/src/app/components/recipe-card/recipe-card.component.ts
+++ client/src/app/components/recipe-card/recipe-card.component.ts
@@ -1,11 +1,21 @@
-import { Component } from '@angular/core';
-
-const DEFAULT_TITLE = 'Salade de légumes';
+import { Component, OnInit } from '@angular/core';
+import { DEFAULT_RECIPES } from '@app/contants/default-recipes';
 @Component({
     selector: 'app-recipe-card',
     templateUrl: './recipe-card.component.html',
     styleUrls: ['./recipe-card.component.scss'],
 })
-export class RecipeCardComponent {
-    title: string = DEFAULT_TITLE;
+export class RecipeCardComponent implements OnInit {
+    title: string;
+    img: string;
+    ingredients: string[];
+    steps: string[];
+
+    ngOnInit(): void {
+        this.title = DEFAULT_RECIPES[0].title;
+        this.img = DEFAULT_RECIPES[0].img;
+        this.ingredients = DEFAULT_RECIPES[0].ingredients;
+        this.steps = DEFAULT_RECIPES[0].steps;
+    }
+
 }
