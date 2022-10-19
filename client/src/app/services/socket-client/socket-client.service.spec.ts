939c78683ee5dc042fa774fdf3e968f9eaec988e
ThierryBeaulieu <thierry.abeaulieu@gmail.com>
2022-10-19 14:59:18 -0400

    fix: adding basic tests for sockets

--- client/src/app/services/socket-client/socket-client.service.spec.ts
+++ client/src/app/services/socket-client/socket-client.service.spec.ts
@@ -17,4 +17,31 @@ describe('SocketClientService', () => {
     it('should be created', () => {
         expect(service).toBeTruthy();
     });
+
+    it('should disconnect', () => {
+        const spy = spyOn(service.socket, 'disconnect');
+        service.disconnect();
+        expect(spy).toHaveBeenCalled();
+    });
+
+    it('isSocketAlive should return true if the socket is still connected', () => {
+        service.socket.connected = true;
+        expect(service.isSocketAlive()).toBeTruthy();
+    });
+
+    it('isSocketAlive should return false if the socket is no longer connected', () => {
+        service.socket.connected = false;
+        expect(service.isSocketAlive()).toBeFalsy();
+    });
+
+    it('isSocketAlive should return false if the socket is not defined', () => {
+        (service.socket as unknown) = undefined;
+        expect(service.isSocketAlive()).toBeFalsy();
+    });
+
+    it('should call socket.on with an event', () => {});
+
+    it('should call emit with data when using send', () => {});
+
+    it('should call emit without data when using send if data is undefined', () => {});
 });
