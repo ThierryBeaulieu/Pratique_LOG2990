b93102e809408d7582b7a7251addd89e56fc2e77
ThierryBeaulieu <thierry.abeaulieu@gmail.com>
2022-10-18 21:53:03 -0400

    fix: adding iteration of the recipes through recipe page

--- client/src/app/pages/recipes-page/recipes-page.component.ts
+++ client/src/app/pages/recipes-page/recipes-page.component.ts
@@ -1,8 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { DEFAULT_RECIPES } from '@app/contants/default-recipes';
+import { Recipe } from '@app/interfaces/recipe';
 
 @Component({
     selector: 'app-recipes-page',
     templateUrl: './recipes-page.component.html',
     styleUrls: ['./recipes-page.component.scss'],
 })
-export class RecipesPageComponent {}
+export class RecipesPageComponent implements OnInit {
+    recipes: Recipe[];
+
+    ngOnInit(): void {
+        this.recipes = DEFAULT_RECIPES;
+    }
+}
